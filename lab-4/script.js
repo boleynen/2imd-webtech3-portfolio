8ccb0c2e2f6aa62e6a30e22d0c3cddb4068ae36c
boleynen <58037455+boleynen@users.noreply.github.com>
2020-03-24 15:57:51 +0100

    added class App & get location function

---
+++ lab-4/script.js
@@ -0,0 +1,51 @@
+// // Create a request variable and assign a new XMLHttpRequest object to it.
+// let request = new XMLHttpRequest();
+
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+//   // Begin accessing JSON data here
+//   request.onload = function(){
+//     let data = JSON.parse(this.response)
+
+//     // check conncention: When an HTTP request is made, the response returns with HTTP status codes. '404' is the most well-known response, meaning Not Found, and '200' OK is a successful request.
+//     if (request.status >= 200 && request.status < 400){
+//         data.forEach(movie =>{
+//             //log each movie's title
+//             console.log(movie.title)
+//         })
+//     }else{
+//         console.log('error')
+//     }
+//   }
+
+class App{
+    constructor(){
+      this.getLocation();
+      this.latitude;
+      this.longitude;
+    }
+  
+    getLocation(){
+      navigator.geolocation.getCurrentPosition(
+        this.myLocation.bind(this),
+        this.noLocation.bind(this)
+      );
+      
+    }
+  
+    myLocation(result){
+      this.latitude = result.coords.latitude;
+      this.longitude = result.coords.longitude;
+      console.log(this.latitude, this.longitude)
+    }
+  
+    noLocation(){
+      console.log('error')
+    }
+  
+  }
+  
+  let app = new App();
+    
+  // request.send()
