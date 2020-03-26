fd797fe8b1a835b8264f0616153eecf5630093f8
Bo <bo_leynen_@hotmail.com>
2020-03-26 16:32:08 +0100

    Added getWeather function

--- lab-4/script.js
+++ lab-4/script.js
@@ -15,6 +15,23 @@ class App{
   gotLocation(result){
     this.lat = result.coords.latitude;
     this.lng = result.coords.longitude;
+    this.getWeather();
+  }
+
+  getWeather(){
+    let url =  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b0244733d0ff63c362982da9006f233c/${this.lat},${this.lng}?units=si`;
+
+    fetch(url)
+      .then(response => {
+        return response.json();
+
+      }).then(data => {
+        console.log("data = ",data);
+        document.querySelector("#weather").innerHTML = data.currently.summary;
+
+      }).catch(err => {
+        console.log("error = ",err);
+      });
   }
 
   noLocation(err){
