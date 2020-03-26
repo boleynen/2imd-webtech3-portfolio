94ba072c5e4f5c2116b0f462136c39254beccef6
Bo <bo_leynen_@hotmail.com>
2020-03-26 21:17:54 +0100

    Added start to getDring function

--- lab-4/script.js
+++ lab-4/script.js
@@ -16,10 +16,11 @@ class App{
     this.lat = result.coords.latitude;
     this.lng = result.coords.longitude;
     this.getWeather();
+    // this.getDrink();
   }
 
   getWeather(){
-    let url =  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/b0244733d0ff63c362982da9006f233c/${this.lat},${this.lng}?units=si`;
+    let url =  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8dfa886df75136ffadb934898c0769ba/${this.lat},${this.lng}?units=si`;
 
     fetch(url)
       .then(response => {
@@ -27,12 +28,29 @@ class App{
 
       }).then(data => {
         console.log("data = ",data);
-        document.querySelector("#weather").innerHTML = data.currently.summary;
+        document.querySelector("#weather").innerHTML = "It's "+ data.currently.summary +" throughout the day!";
 
       }).catch(err => {
         console.log("error = ",err);
       });
   }
+
+  // getDrink(){
+    
+  //   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  //   .then(response => {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then(data =>{
+  //     // console.log(data);
+  //     // console.log(data.drinks[0].strDrink);
+  //     document.querySelector("#drink").innerHTML = "Grab a "+data.drinks[0].strDrink+ " quickly!"
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  // }
 
   noLocation(err){
     console.log(err);
