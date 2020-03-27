796bd3e934a42531ea09197d0140f34d93db0ae2
boleynen <58037455+boleynen@users.noreply.github.com>
2020-03-27 14:20:01 +0100

    added files to desktop pc

--- lab-4/script.js
+++ lab-4/script.js
@@ -1,64 +1,101 @@
-// // Create a request variable and assign a new XMLHttpRequest object to it.
-// let request = new XMLHttpRequest();
-
-// // Open a new connection, using the GET request on the URL endpoint
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-//   // Begin accessing JSON data here
-//   request.onload = function(){
-//     let data = JSON.parse(this.response)
-
-//     // check conncention: When an HTTP request is made, the response returns with HTTP status codes. '404' is the most well-known response, meaning Not Found, and '200' OK is a successful request.
-//     if (request.status >= 200 && request.status < 400){
-//         data.forEach(movie =>{
-//             //log each movie's title
-//             console.log(movie.title)
-//         })
-//     }else{
-//         console.log('error')
-//     }
-//   }
-
-class App{
-    constructor(){
-      this.getLocation();
-      this.latitude;
-      this.longitude;
-    }
-  
-    getLocation(){
-      navigator.geolocation.getCurrentPosition(
-        this.myLocation.bind(this),
-        this.noLocation.bind(this)
-      );
-      
-    }
-  
-    myLocation(result){
-      this.latitude = result.coords.latitude;
-      this.longitude = result.coords.longitude;
-      console.log(this.latitude, this.longitude)
-    }
-  
-    noLocation(){
-      console.log('error')
-    }
-
-    getWeather(){
-      let url = `https://api.darksky.net/forecast/befdca694f1063bb1ae93f5046e7abd5/` + this.latitude + `,` + this.longitude;
-      // console.log(url);
-      fetch(url)
-          .then(response => {
-             return response.json();
-          }).then(data => {
-              document.querySelector(".h1").innerHTML = data.currently.summary;
-          }) .catch(error=>{
-              console.log(error);
-          })
-    }
-  
+class App {
+  constructor() {
+    this.getLocation();
+    this.lat;
+    this.lng;
   }
-  
-  let app = new App();
-    
-  // request.send()
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      this.gotLocation.bind(this),
+      this.noLocation.bind(this)
+    );
+  }
+
+  gotLocation(result) {
+    this.lat = result.coords.latitude;
+    this.lng = result.coords.longitude;
+    this.getWeather();
+    this.getDrink();
+  }
+
+  getWeather() {
+    let temperature = localStorage.getItem("temperature");
+    setTimeout(() => {
+      localStorage.removeItem("temperature");
+    }, 1000 * 60 * 60);
+    if (temperature == null || temperature == "null") {
+
+      let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8dfa886df75136ffadb934898c0769ba/${this.lat},${this.lng}?units=si`;
+
+      fetch(url)
+        .then(response => {
+          return response.json();
+
+        }).then(data => {
+          temperature = localStorage.setItem("temperature", (data.currently.temperature));
+          console.log("weather data = ", data);
+          document.querySelector("#degrees").innerHTML = "It's " + data.currently.temperature + "&#176; today!";
+
+        }).catch(err => {
+          console.log("error = ", err);
+        });
+
+    } else {
+      console.log("data from storage");
+      console.log(localStorage);
+      document.querySelector("#degrees").innerHTML = "It's " + localStorage.getItem("temperature") + "&#176; today!";
+    }
+  }
+
+  getDrink() {
+    let degrees = document.querySelector("#degrees").innerText;
+    // let degrees = 25;
+    if (degrees >= 20) {
+
+      fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then(response => {
+          // console.log(response);
+          return response.json();
+        })
+        .then(data => {
+          // console.log(data);
+          // console.log(data.drinks[0].strDrink);
+          console.log("too hot!");
+          document.querySelector("#drink").innerHTML = "It's too hot ! Cool off with an ice cold " + data.drinks[1].strDrink + "!";
+          document.querySelector("#container").setAttribute("class", "strawberry");
+          document.querySelector("#weather").setAttribute("class", "strawberryH1");
+
+        })
+        .catch(err => {
+          console.log(err);
+        })
+
+    } else {
+      
+      fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+        .then(response => {
+          // console.log(response);
+          return response.json();
+        })
+        .then(data => {
+          // console.log(data);
+          // console.log(data.drinks[0].strDrink);
+          console.log("too cold!");
+          document.querySelector("#drink").innerHTML = "Brr, it's kinda cold outside.. But you decide when summer starts, with a sweet " + data.drinks[4].strDrink + " in your hands.";
+          document.querySelector("#container").setAttribute('class', "blue");
+          document.querySelector("#weather").setAttribute("class", "blueH1");
+
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  }
+
+  noLocation(err) {
+    console.log(err);
+  }
+}
+
+let app = new App();
