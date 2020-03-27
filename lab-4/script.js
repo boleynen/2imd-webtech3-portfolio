18d012fd2a721e3df876e4b204b998bd17f2151b
Bo <bo_leynen_@hotmail.com>
2020-03-27 10:42:21 +0100

    fixed errors

--- lab-4/script.js
+++ lab-4/script.js
@@ -16,7 +16,7 @@ class App{
     this.lat = result.coords.latitude;
     this.lng = result.coords.longitude;
     this.getWeather();
-    // this.getDrink();
+    this.getDrink();
   }
 
   getWeather(){
@@ -35,22 +35,22 @@ class App{
       });
   }
 
-  // getDrink(){
+  getDrink(){
     
-  //   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-  //   .then(response => {
-  //     console.log(response);
-  //     return response.json();
-  //   })
-  //   .then(data =>{
-  //     // console.log(data);
-  //     // console.log(data.drinks[0].strDrink);
-  //     document.querySelector("#drink").innerHTML = "Grab a "+data.drinks[0].strDrink+ " quickly!"
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-  // }
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data =>{
+      console.log(data);
+      console.log(data.drinks[0].strDrink);
+      document.querySelector("#drink").innerHTML = "Grab a "+data.drinks[0].strDrink+ " quickly!"
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 
   noLocation(err){
     console.log(err);
