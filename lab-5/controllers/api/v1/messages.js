013203fd8dfa06451be6eec69d51b4057047e3db
boleynen <58037455+boleynen@users.noreply.github.com>
2020-03-29 21:14:20 +0200

    json

--- lab-5/controllers/api/v1/messages.js
+++ lab-5/controllers/api/v1/messages.js
@@ -1,21 +1,50 @@
+// GET:     /api/v1/messages - geeft messages terug
 const getAll = (req, res) =>{
-    res.send("get messages");
+    res.json({
+        "status": "success",
+        "message": "getting messages"
+    })
 }
 
+// GET:     /api/v1/messages/:id - geeft message met specifiek ID terug
 const getOne = (req, res) =>{
-    res.send("get message " + req.params.id);
+    res.json({
+        "status": "success",
+        "message": "getting message with id " + req.params.id
+    })
 }
 
+// POST:    /api/v1/messages - kan een JSON-object ontvangen en bewaren en geeft het nieuwe document terug
 const postAll = (req, res) =>{
-    res.send("post messages");
+    res.json({
+        "status": "success",
+        "message": "posting messages"
+    })
 }
 
+// PUT:     /api/v1/messages/:id - kan een JSON-object ontvangen en een specifiek bericht updaten en geeft die nieuwe bericht terug
 const update = (req, res) =>{
-    res.send("Update message " + res.params.id);
+    res.json({
+        "status": "success",
+        "message": "updating message with id " + req.params.id
+    })
 }
 
+// DELETE:  /api/v1/messages/:id - kan een message met id verwijderen en geeft een response terug
 const remove = (req, res) =>{
-    res.send("Delete message " + req.params.id);
+    res.json({
+        "status": "success",
+        "message": "removing message with id " + req.params.id
+    })
+}
+
+// GET:     /api/v1/messages?user=username - kan berichten teruggeven voor een bepaalde username
+
+const getAllUser = (req, res) => {
+    res.json({
+        "status": "success",
+        "message": "getting messages from user " + req.params.user
+    })
 }
 
 module.exports.getAll = getAll;
@@ -23,4 +52,5 @@ module.exports.getOne = getOne;
 module.exports.postAll = postAll;
 module.exports.update = update;
 module.exports.remove = remove;
+module.exports.getAllUser = getAllUser;
 
--- lab-5/routers/api/v1/messages.js
+++ lab-5/routers/api/v1/messages.js
@@ -6,20 +6,19 @@ const messagesController = require('../../../controllers/api/v1/messages.js');
 router.get('/', messagesController.getAll);
 
 // GET:     /api/v1/messages/:id - geeft message met specifiek ID terug
-router.get('/:id', messagesController.getOne)
+router.get('/:id', messagesController.getOne);
 
 // POST:    /api/v1/messages - kan een JSON-object ontvangen en bewaren en geeft het nieuwe document terug
-router.post('/', messagesController.postAll)
+router.post('/', messagesController.postAll);
 
 // PUT:     /api/v1/messages/:id - kan een JSON-object ontvangen en een specifiek bericht updaten en geeft die nieuwe bericht terug
-router.put('/:id', messagesController.update)
+router.put('/:id', messagesController.update);
 
 // DELETE:  /api/v1/messages/:id - kan een message met id verwijderen en geeft een response terug
-router.delete('/:id', messagesController.remove)
+router.delete('/:id', messagesController.remove);
 
 // GET:     /api/v1/messages?user=username - kan berichten teruggeven voor een bepaalde username
-// router.get('/api/v1/messages?user=:username', (req, res) =>{
-//     res.send("get message of " + req.params.username);
-// })
+router.get("/?user=:username", messagesController.getAllUser);
+
 
 module.exports = router;
