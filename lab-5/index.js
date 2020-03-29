1df7a34ceb0a37b13164bea3384b3b8b29e493ad
boleynen <58037455+boleynen@users.noreply.github.com>
2020-03-29 16:36:32 +0200

    Added application skeleton using Express

--- lab-5/index.js
+++ lab-5/index.js
@@ -1,8 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// var express = require('express');
+// var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get('/messages', function (req, res) {
+// 	res.send("GET messages");
+// });
 
+// app.get('/messages/:id', function (req, res) {
+// 	var id = req.params.id;
+// 	res.send("GET message with :id " + id);
+// });
+
+// app.post('/messages', function (req, res){
+// 	res.send("POST messages");
+// });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
+
+var logger = require('./logger.js');
+logger('message');
