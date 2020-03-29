ce5c9804db1f40b74e9cb418dca7445a1f20477a
boleynen <58037455+boleynen@users.noreply.github.com>
2020-03-29 18:33:52 +0200

    updated route-structure of app.js

--- lab-5/index.js
+++ lab-5/index.js
@@ -1,27 +1,43 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+// test with hello world
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-// app.get('/messages', function (req, res) {
-// 	res.send("GET messages");
-// });
+// show messages from MongoDB
+app.get('/messages', function (req, res) {
+	res.send("GET messages");
+});
 
-// app.get('/messages/:id', function (req, res) {
-// 	var id = req.params.id;
-// 	res.send("GET message with :id " + id);
-// });
+// show message with specific ID from MongoDB
+app.get('/api/v1/messages/:id', function (req, res) {
+	var id = req.params.id;
+	res.send("GET message with :id " + id);
+});
 
-// app.post('/messages', function (req, res){
-// 	res.send("POST messages");
-// });
+// gets and saves JSON object and sends new document back
+app.post('/api/v1/messages', function (req, res){
+	res.send("POST messages");
+});
 
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
+// update a message from MongoDB
+app.put("/api/v1/messages/:id", (req, res) => {
+    res.send("Update message " + res.params.id);
+})
 
+// delete message with specific ID from MongoDB
+app.delete("/api/v1/messages/:id", (req, res) => {
+    res.send("Delete message " + req.params.id);
+})
 
-var logger = require('./logger.js');
-logger('message');
+// shows messages of user with specific ID
+app.get("/api/v1/messages?user=:username", (req, res) => {
+    res.send("get message of " + req.params.username);
+})
+
+// log path to port
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
